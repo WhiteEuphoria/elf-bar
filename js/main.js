829656b9f088265499f4e935d48e985857954166
whiteEuphoria <prognikitos@gmail.com>
2021-12-20 13:11:31 +0200

    create burger menu and animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const burgerBtn = document.querySelector(".burger")
 const removeClass = document.querySelector("#link")
+const buyBtn = document.querySelector("#buy")
+const closeBtn = document.querySelector("#close")
 
 burgerBtn.addEventListener("click", function (){
     document.querySelector(".navbar").classList.toggle("navbar--visible");
@@ -7,10 +9,17 @@ burgerBtn.addEventListener("click", function (){
     document.querySelector(".burger__phone").classList.toggle("burger__phone--visible");
     document.querySelector(".burger").classList.toggle("burger--active");
 });
-removeClass
-    .addEventListener("click", function (){
+removeClass.addEventListener("click", function (){
     document.querySelector(".navbar").classList.remove("navbar--visible");
     document.querySelector(".header").classList.remove("burger--background");
     document.querySelector(".burger__phone").classList.remove("burger__phone--visible");
     document.querySelector(".burger").classList.remove("burger--active");
-    })
+})
+
+buyBtn.addEventListener("click", function(){
+    document.querySelector(".back").classList.add("rotate-1");
+});
+
+closeBtn.addEventListener("click", function(){
+    document.querySelector(".back").classList.remove("rotate-1");
+});
